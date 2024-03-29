6069740fc6d763322fd67a6d25af96eff7867448
anomaddev <anomaddev@gmail.com>
2024-03-29 00:05:25 -0400

    working

--- Sources/NomadUI/Controllers/RoutingController.swift
+++ Sources/NomadUI/Controllers/RoutingController.swift
@@ -5,16 +5,71 @@
 //  Created by Justin Ackermann on 3/26/24.
 //
 
+// Core iOS
 import UIKit
+
+// Utilities
 import FAPanels
+import Cartography
+import NVActivityIndicatorView
 
 open class RoutingController: FAPanelController {
     
+    public static var defaultConfig: FAPanelConfigurations = {
+        var configs = FAPanelConfigurations()
+        configs.colorForTapView = UIColor.black.withAlphaComponent(0.1)
+        configs.shouldAnimateWithPan = true
+        configs.leftPanelWidth = 300
+        configs.rightPanelWidth = 200
+        configs.resizeLeftPanel = true
+        configs.resizeRightPanel = true
+        configs.canLeftSwipe = false
+        configs.canRightSwipe = false
+        configs.bounceOnLeftPanelOpen = false
+        configs.bounceOnRightPanelOpen = true
+        configs.maxAnimDuration = 0.25
+        configs.centerPanelTransitionType = .crossDissolve
+        configs.shadowColor = UIColor.clear.cgColor
+        configs.unloadLeftPanel = false
+        configs.unloadRightPanel = false
+        configs.pusheSidePanels = false
+        return configs
+    }()
+    
+    //    public var leftMenuWidth: CGFloat   = (UIScreen.main.bounds.width / 2) + 50
+    //    { didSet { configs.leftPanelWidth = leftMenuWidth }}
+    //
+    //    public var rightMenuWidth: CGFloat  = UIScreen.main.bounds.width * 3/5
+    //    { didSet { configs.rightPanelWidth = rightMenuWidth }}
+    
     public var palatte: UIPalette = UITheme.main.active()
+    
+    init(withConfig configs: FAPanelConfigurations) {
+        super.init()
+        self.configs = configs
+        leftPanelPosition = .front
+        rightPanelPosition = .back
+    }
+    
+    override public init() {
+        super.init()
+        defaultSetup()
+    }
     
     override open func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = palatte.background.color
         
+        
     }
+    
+    private func defaultSetup() {
+        configs = Self.defaultConfig
+        leftPanelPosition = .front
+        rightPanelPosition = .back
+    }
+    
+    required public init?(coder: NSCoder)
+    { super.init(coder: coder) }
 }
+
--- Sources/NomadUI/UITheme/UITheme.swift
+++ Sources/NomadUI/UITheme/UITheme.swift
@@ -14,17 +14,29 @@ public class UITheme: NSObject {
     public var light:   UIPalette = .defaultLight
     public var dark:    UIPalette = .defaultDark
     
+    public var style: UIUserInterfaceStyle {
+        if #available(iOS 13.0, *) {
+            return UITraitCollection.current.userInterfaceStyle
+        } else {
+            return .light
+        }
+    }
+    
     private override init() {
         super.init()
     }
     
-    public func setTheme(basic theme: UIPalette) {
-        self.light = theme
+    public func setTheme(palette: UIPalette) {
+        switch palette.adaptive {
+        case .light:    light = palette
+        case .dark:     dark = palette
+        }
     }
     
     public func active() -> UIPalette {
         if #available(iOS 13.0, *) {
-            return UITraitCollection.current.userInterfaceStyle == .dark ? dark : light
+            print(style.rawValue)
+            return style == .dark ? dark : light
         } else {
             return light
         }
