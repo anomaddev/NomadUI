51228f3f7d34f9f703cd3c8c2106082aec8b40ee
anomaddev <anomaddev@gmail.com>
2024-05-12 23:57:21 -0400

    working

--- Sources/NomadUI/Extensions/NomadUI+UIButton.swift
+++ Sources/NomadUI/Extensions/NomadUI+UIButton.swift
@@ -10,24 +10,30 @@ import UIKit
 @available(iOS 15.0, *)
 extension UIButton.Configuration {
     
-    public static let FacebookLogin: UIButton.Configuration = {
-        var container: AttributeContainer = .BaseButtonConfig
-        
+    fileprivate static func Login() -> UIButton.Configuration {
         var config = UIButton.Configuration.filled()
         config.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
-        config.background.backgroundColor = .FacebookBlue
         config.background.cornerRadius = 5
-        config.attributedTitle = AttributedString(
-            "Login with Facebook",
-            attributes: container
-        )
         
-        config.image = UIImage(resource: .facebookSm)
         config.titleAlignment = .leading
         config.preferredSymbolConfigurationForImage = .init(pointSize: 20)
         config.imagePadding = 20
         config.imagePlacement = .trailing
-        config.buttonSize = .small
+        config.buttonSize = .medium
+        
+        return config
+    }
+    
+    public static let FacebookLogin: UIButton.Configuration = {
+        var container: AttributeContainer = .BaseButtonConfig
+        
+        var config: UIButton.Configuration = .Login()
+        config.background.backgroundColor = .FacebookBlue
+        config.image = UIImage(resource: .facebookSm)
+        config.attributedTitle = AttributedString(
+            "Login with Facebook",
+            attributes: container
+        )
         
         return config
     }()
@@ -35,21 +41,13 @@ extension UIButton.Configuration {
     public static let AppleLogin: UIButton.Configuration = {
         var container: AttributeContainer = .BaseButtonConfig
         
-        var config = UIButton.Configuration.filled()
-        config.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
+        var config: UIButton.Configuration = .Login()
         config.background.backgroundColor = .black
-        config.background.cornerRadius = 5
+        config.image = UIImage(resource: .appleSm)
         config.attributedTitle = AttributedString(
             "Login with Apple",
             attributes: container
         )
-        
-        config.image = UIImage(resource: .appleSm)
-        config.titleAlignment = .leading
-        config.preferredSymbolConfigurationForImage = .init(pointSize: 20)
-        config.imagePadding = 20
-        config.imagePlacement = .trailing
-        config.buttonSize = .small
         
         return config
     }()
