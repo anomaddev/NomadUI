ae615da3efb86555166cf1548c2c23eabbb1000d
anomaddev <anomaddev@gmail.com>
2024-05-12 22:23:53 -0400

    working

--- Sources/NomadUI/Extensions/NomadUI+UIButton.swift
+++ Sources/NomadUI/Extensions/NomadUI+UIButton.swift
@@ -11,15 +11,16 @@ import UIKit
 extension UIButton.Configuration {
     
     public static let AppleLogin: UIButton.Configuration = {
+        var container = AttributeContainer()
+        container.foregroundColor = .primary.onColor
+        container.font = Font.Regular.getFont(size: 16)
+        
         var config = UIButton.Configuration.plain()
         config.background.backgroundColor = .FacebookBlue
         config.background.cornerRadius = 5
         config.attributedTitle = AttributedString(
             "Login with Facebook",
-            attributes: AttributeContainer([
-                Font.Key: Font.Regular.getFont(size: 16),
-                UIColor.Foreground: UIColor.primary.onColor
-            ])
+            attributes: container
         )
         
         return config
