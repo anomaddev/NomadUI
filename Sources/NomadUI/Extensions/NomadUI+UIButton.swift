21a676ba292b6b1f5e6d53cd395f501eb0c91a01
anomaddev <anomaddev@gmail.com>
2024-05-12 23:52:29 -0400

    working

--- Sources/NomadUI/Extensions/NomadUI+UIButton.swift
+++ Sources/NomadUI/Extensions/NomadUI+UIButton.swift
@@ -10,7 +10,7 @@ import UIKit
 @available(iOS 15.0, *)
 extension UIButton.Configuration {
     
-    public static let AppleLogin: UIButton.Configuration = {
+    public static let FacebookLogin: UIButton.Configuration = {
         var container: AttributeContainer = .BaseButtonConfig
         
         var config = UIButton.Configuration.filled()
@@ -32,6 +32,27 @@ extension UIButton.Configuration {
         return config
     }()
     
+    public static let AppleLogin: UIButton.Configuration = {
+        var container: AttributeContainer = .BaseButtonConfig
+        
+        var config = UIButton.Configuration.filled()
+        config.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
+        config.background.backgroundColor = .black
+        config.background.cornerRadius = 5
+        config.attributedTitle = AttributedString(
+            "Login with Apple",
+            attributes: container
+        )
+        
+        config.image = UIImage(resource: .appleSm)
+        config.titleAlignment = .leading
+        config.preferredSymbolConfigurationForImage = .init(pointSize: 20)
+        config.imagePadding = 20
+        config.imagePlacement = .trailing
+        config.buttonSize = .small
+        
+        return config
+    }()
 }
 
 @available(iOS 15, *)
